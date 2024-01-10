fd4e1ec7a98e4625b90f5bd86bac1b229dbada69
gh_work <andrew_cetnarskyj@standardlife.com>
2024-01-10 16:30:06 +0000

    Added config local

--- exporters/css/config.ts
+++ exporters/css/config.ts
@@ -1,4 +1,4 @@
-import { pathCase, ColorFormat } from "@supernovaio/export-helpers"
+import { stringCase, ColorFormat } from "@supernovaio/export-helpers"
 import { TokenType } from "@supernovaio/sdk-exporters"
 
 /**
@@ -18,7 +18,7 @@ export type ExporterConfiguration = {
   /** When enabled, values will use references to other tokens where applicable */
   useReferences: boolean
   /** Style of exported token names */
-  tokenNameStyle: pathCase
+  tokenNameStyle: stringCase
   /** Format of the exported colors */
   colorFormat: ColorFormat
   /** Max number of decimals in colors */
